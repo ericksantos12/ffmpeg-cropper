12d9fed1a3493a9d2250599d9acaebcdfee1f019
Erick Santos <erick.sousa@hotmail.com.br>
2023-01-21 23:42:22 -0300

    ✨ added menu and mp4 to mp3 converter

--- app.py
+++ app.py
@@ -9,17 +9,20 @@ from rich.layout import Layout
 from rich.traceback import install
 install()
 
-# progress = Progress()
-
-# TODO implement menu()
-#   TODO implement convert to mp3
 
 OUTPUT_DIR = "./output"
 console = Console()
 
 
 def menu():
-    pass
+    options = """Select an option:
+    [blue]1[/] - Cut timestamps
+    [blue]2[/] - Convert output folder to mp3
+    [blue]3[/] - Do all from above
+    [blue]0[/] - Exit"""
+
+    console.print(Panel(options, title="[bold]Menu"))
+    return int(console.input("[green bold]> "))
 
 
 def ffmpeg_cut(video_path, output_path=OUTPUT_DIR):
@@ -35,31 +38,60 @@ def ffmpeg_cut(video_path, output_path=OUTPUT_DIR):
                 subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
 
+def ffmpeg_mp4_to_mp3(output_path=OUTPUT_DIR):
+    audio_path = os.path.abspath(output_path + "/mp3/")
 
-            # progress.log(f"Cutting {filename} from {start} to {end}")
-            subprocess.run(cmd, stdout=subprocess.DEVNULL,
-                           stderr=subprocess.STDOUT)
+    create_dir(audio_path)
+
+    for filename in track(os.listdir(output_path), description="Converting..."):
+
+        output_video_path = os.path.join(output_path, filename)
+        output_audio_path = os.path.join(audio_path, filename[:-4] + ".mp3")
+
+        if filename.endswith(".mp4") and not os.path.isfile(output_audio_path):
+            cmd = ["ffmpeg", "-i", output_video_path, output_audio_path]
+
+            subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
 
-def ffmpeg_convert_to_mp3(video_path, output_path=OUTPUT_DIR):
-    pass
+def prompt_cut():
+    while True:
+        video_path = os.path.abspath(Prompt.ask("Enter the path of the video file"))
+        if os.path.isfile(video_path):
+            console.print(f'[bold green]Video file found![/bold green] [blue]{os.path.basename(video_path)}[/blue]')
+            break
+
+        console.print('[bold red]Video file not found![/bold red]')
+
+    create_dir(OUTPUT_DIR)
+
+    ffmpeg_cut(video_path)
+
+
+def prompt_mp4_mp3():
+    create_dir(OUTPUT_DIR)
+    ffmpeg_mp4_to_mp3(OUTPUT_DIR)
+
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
 
 
 def main():
+
     while True:
-        video_path = os.path.abspath(
-            input("Enter the path of the video file: "))
-        if os.path.isfile(video_path):
-            print(
-                f'[bold green]Video file found![/bold green] [blue]{os.path.basename(video_path)}[/blue]')
-            break
-
-        print('[bold red]Video file not found![/bold red]')
-
-    if not os.path.exists(OUTPUT_DIR):
-        os.mkdir(OUTPUT_DIR)
-        
-    ffmpeg_cut(video_path)
+        key = menu()
+        match key:
+            case 1:
+                prompt_cut()
+            case 2:
+                prompt_mp4_mp3()
+            case 3:
+                prompt_cut()
+                prompt_mp4_mp3()
+            case 0:
+                exit()
 
 
 if __name__ == '__main__':
