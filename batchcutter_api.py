b41784695142122eedd19d674cbe208ae3dab2c9
Erick Sousa <16109127+ericksantos12@users.noreply.github.com>
2023-10-30 14:08:38 -0300

    🚚 renamed app.py > batchcutter_api

--- batchcutter_api.py
+++ batchcutter_api.py
@@ -4,26 +4,12 @@ import os
 from rich.console import Console
 from rich.prompt import Prompt
 from rich.progress import track
-from rich.panel import Panel
-from rich.layout import Layout
 from rich.traceback import install
 install()
 
 
 OUTPUT_DIR = "./output"
 console = Console()
-
-
-def menu():
-    options = """Select an option:
-    [blue]1[/] - Cut timestamps
-    [blue]2[/] - Convert output folder to mp3
-    [blue]3[/] - Do all from above
-    [blue]0[/] - Exit"""
-
-    console.print(Panel(options, title="[bold]Menu"))
-    return int(console.input("[green bold]> "))
-
 
 def ffmpeg_cut(video_path, output_path=OUTPUT_DIR):
     with open("cuts.csv") as f:
@@ -58,10 +44,10 @@ def prompt_cut():
     while True:
         video_path = os.path.abspath(Prompt.ask("Enter the path of the video file"))
         if os.path.isfile(video_path):
-            console.print(f'[bold green]Video file found![/bold green] [blue]{os.path.basename(video_path)}[/blue]')
+            console.log(f'Video file found! {os.path.basename(video_path)}')
             break
 
-        console.print('[bold red]Video file not found![/bold red]')
+        console.log('Video file not found!')
 
     create_dir(OUTPUT_DIR)
 
@@ -76,23 +62,3 @@ def prompt_mp4_mp3():
 def create_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
-
-
-def main():
-
-    while True:
-        key = menu()
-        match key:
-            case 1:
-                prompt_cut()
-            case 2:
-                prompt_mp4_mp3()
-            case 3:
-                prompt_cut()
-                prompt_mp4_mp3()
-            case 0:
-                exit()
-
-
-if __name__ == '__main__':
-    main()
